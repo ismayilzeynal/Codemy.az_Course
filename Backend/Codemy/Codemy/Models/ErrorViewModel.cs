e42b3ab41a21c3bcbeb9e9387ade930dbc17c9a9
ismayilzeynal <ismayilbz@code.edu.az>
2023-07-31 18:54:26 +0400

    feature (b): backend init

---
+++ Backend/Codemy/Codemy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Codemy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
